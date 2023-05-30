38d3f66ef3a63e141fe9534b0d6efe42cd8de48e
Tam Nguyen <ngthithutam910@gmail.com>
2023-05-30 16:36:51 +0700

    update: mọi người thêm file giao diện có đuôi là .txs và file src

--- AwesomeProject/index.js
+++ AwesomeProject/index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import Topping from './src/Topping';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Topping);
---
+++ AwesomeProject/src/Topping.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import type {PropsWithChildren} from 'react';
+import { Button, Text, View } from "react-native";
+
+function Topping(): JSX.Element{
+    return <View>
+        {/* <Button></Button> */}
+    </View>
+}
+
+export default Topping;
