86005b3a3caba50374cf44abc1f002a6c0f50f90
AnhPhatt <86426105+AnhPhatt@users.noreply.github.com>
2023-06-02 17:27:49 +0700

    .

---
+++ AwesomeProject/index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import Topping from './src/Topping';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => Topping);
---
+++ AwesomeProject/src/Topping.tsx
@@ -0,0 +1,11 @@
+import React from "react-native"
+import type {PropsWithChildren} from 'react';
+import { Button, Text, View } from "react-native";
+
+function Topping(): JSX.Element{
+    return(
+        <View><Text>áklflasfa</Text></View>
+    )
+};
+
+export default Topping;
