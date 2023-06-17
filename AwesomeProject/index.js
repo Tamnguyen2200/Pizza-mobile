f3ed64503255c490301b7ef8d60c4e4c762fa382
Tam Nguyen <ngthithutam910@gmail.com>
2023-06-17 08:55:26 +0700

    chỉnh sữa lỗi khi gộp nhánh

--- AwesomeProject/index.js
+++ AwesomeProject/index.js
@@ -8,12 +8,5 @@ import {name as appName} from './app.json';
 import Begin from'./src/Begin';
 import Login from'./src/Login';
 import Signin from'./src/Signin';
-<<<<<<< HEAD
 import Payment from './src/Payment'
 AppRegistry.registerComponent(appName, () => Payment);
-=======
-import Home from './src/Home';
-import Search from './src/Search.tsx'
-
-AppRegistry.registerComponent(appName, () => Search);
->>>>>>> phat.huynh
