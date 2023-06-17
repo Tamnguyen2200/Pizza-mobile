03e2eaf508126bae27a903c52a60921495f2a13b
Chr-Nolan <133561049+Chr-Nolan@users.noreply.github.com>
2023-06-17 07:50:24 +0700

    update: thêm UI payment

--- AwesomeProject/index.js
+++ AwesomeProject/index.js
@@ -7,5 +7,6 @@ import Topping from './src/Topping';
 import {name as appName} from './app.json';
 import Begin from'./src/Begin';
 import Login from'./src/Login';
-import Signin from'./src/Signin'
-AppRegistry.registerComponent(appName, () => Topping);
+import Signin from'./src/Signin';
+import Payment from './src/Payment'
+AppRegistry.registerComponent(appName, () => Payment);
